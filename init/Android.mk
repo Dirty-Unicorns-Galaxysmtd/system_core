Haus1/android_system_core

Init: Add support for mounting emmc partitions by name
Allow init to mount partitions by their labels on emmc devices. This brings it in line
with mtd devices who already support this.

This initial implementation respects the BOARD_VOLD_MAX_PARTITIONS build flag,
but is limited to partitions on mmc block zero.

Change-Id: I7b30e8beb7d159d920c9eb87d7b9e42094da2649

init/Android.mk
@@ -40,6 +40,10 @@ ifneq ($(TARGET_NR_SVC_SUPP_GIDS),)
 LOCAL_CFLAGS += -DNR_SVC_SUPP_GIDS=$(TARGET_NR_SVC_SUPP_GIDS)
 endif
 
+ifneq ($(BOARD_VOLD_MAX_PARTITIONS),)
+common_cflags += -DMAX_EMMC_PARTITIONS=$(BOARD_VOLD_MAX_PARTITIONS)
+endif
+
 SYSTEM_CORE_INIT_DEFINES := BOARD_CHARGING_MODE_BOOTING_LPM \
     BOARD_LPM_BOOT_ARGUMENT_NAME \
     BOARD_LPM_BOOT_ARGUMENT_VALUE

Dirty-Unicorns-Galaxysmtd/system_core

init/Android.mk
@@ -40,6 +40,10 @@ ifneq ($(TARGET_NR_SVC_SUPP_GIDS),)
 LOCAL_CFLAGS += -DNR_SVC_SUPP_GIDS=$(TARGET_NR_SVC_SUPP_GIDS)
 endif
 
+ifneq ($(BOARD_VOLD_MAX_PARTITIONS),)
+LOCAL_CFLAGS += -DMAX_EMMC_PARTITIONS=$(BOARD_VOLD_MAX_PARTITIONS)
+endif
+
 SYSTEM_CORE_INIT_DEFINES := BOARD_CHARGING_MODE_BOOTING_LPM \
     BOARD_LPM_BOOT_ARGUMENT_NAME \
     BOARD_LPM_BOOT_ARGUMENT_VALUE